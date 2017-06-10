56dc5c2431ec69e5197be62d979955bdde5f218e
Fredrik Mandal <fmandal@gmail.com>
2017-06-11 01:53:53 +0200

    Updated readme.

--- MMM-MJPG-stream.js
+++ MMM-MJPG-stream.js
@@ -2,10 +2,10 @@ Module.register('MMM-MJPG-stream', {
     defaults: {
         title: 'MJPG Stream',
         header: false,
-        url: '',
         width: '100%',
-        height: '300px',
-        refreshInterval: 60000
+        height: '480px',
+        moveUp: '60px;',
+        refreshInterval: 60 // IMPORTANT: Refresh interval is to be set in seconds, not millisecs.
     },
 
     getTranslations: function() {
@@ -30,27 +30,23 @@ Module.register('MMM-MJPG-stream', {
         var self = this;
         setInterval(function() {
             self.updateDom();
-        }, 1000);
+        }, 5000);
     },
 
     getDom: function() {
-        var wrapper = document.createElement('div');
-        if(!this.loaded){
-            wrapper.innerHTML = this.translate('LOADING');
-            wrapper.className = "dimmed light small";
-            return wrapper;
-        }
-
-        if(this.config.header){
-            var header = document.createElement('header');
-            header.innerHTML = this.config.title;
-            header.className = 'align-left';
-            wrapper.appendChild(header);
-        }
-        
-        wrapper.appendChild(this.loadUrl());
-
-        return wrapper;
+        var content = document.createElement('iframe');
+        content.style.border = "0";
+        content.style.background = "#00ff00";
+        content.style.width = this.config.width;
+        content.style.height = this.config.height;
+        var html = '<html><head><meta http-equiv="refresh" content="' + this.config.refreshInterval + '"><title>Content</title></head><body style="margin:0;padding:0;">';
+        html += '<div style="width:60%;height:100%;background: #0f0 url(\'' + this.config.url1 + '\') no-repeat fixed center top; float:left;';
+        html += ' background-position: 70% -125px; transform: rotate(180deg); background-size: 1280px 960px"></div>';
+        html += '<div style="width:40%;height:100%;background: #f0f url(\'' + this.config.url2 + '\') no-repeat fixed center center; float:right;';
+        html += ' background-position: 101% 100%; background-size: 640px 480px;"></div>';
+        html += '</body></html>';
+        content.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+        return content;
     },
-    
-    
+
+});
